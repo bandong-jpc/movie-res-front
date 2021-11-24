610f17e47893538729a8637b35dcab7407acfa20
MikmikMikmikMikmik <92725990+MikmikMikmikMikmik@users.noreply.github.com>
2021-11-24 13:39:19 +0800

    added app_seatRest
    app_seatRest folder contains seat reservation page

---
+++ src/app/app_seatRes/app.seatRes.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seat',
+  templateUrl: './app/app_seatRes/app.seatRes.html',
+  styleUrls: ['./app/app_seatRes/app.seatRes.css']
+})
+export class SeatReserve {
+  body = 'movie-res-front';
+}
